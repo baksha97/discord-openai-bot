1844aa39d9fde5113aecb7227f451c52d4b2e54d
Alfayed <baksha97@gmail.com>
2022-03-30 17:29:01 -0400

    refactor to data classes

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ import discord
 import openai
 from dotenv import dotenv_values
 
-from aiconfig import generate_response, ENGINES
+from aiconfig import generate_response, AVAILABLE_ENGINES
 
 config = dotenv_values(".env")
 OPEN_AI_TOKEN = config.get('OPEN_AI_TOKEN')
@@ -21,17 +21,17 @@ async def on_ready():
 async def on_message(message):
     if message.content.startswith('$help'):
         message_reply = ''
-        for engine_name in ENGINES.keys():
-            message_reply += f'`${engine_name}`: {ENGINES[engine_name]["description"]}\n'
+        for engine in AVAILABLE_ENGINES:
+            message_reply += f'`${engine.name}`: {engine.description}\n'
         message_reply += 'Usage: `$<engine_name> <actual prompt>`.\n'
         message_reply += 'Costs: `davinci` > `curie` > `babbage` > `ada`.'
         await message.channel.send(message_reply)
         return
 
-    for engine_name in ENGINES.keys():
-        if message.content.startswith(f'${engine_name} '):
-            prompt = message.content.replace(f'${engine_name} ', '')
-            message_reply = generate_response(prompt=prompt, engine=engine_name)
+    for engine in AVAILABLE_ENGINES:
+        if message.content.startswith(f'${engine.name} '):
+            prompt = message.content.replace(f'${engine.name} ', '')
+            message_reply = generate_response(prompt=prompt, engine=engine)
             await message.channel.send(message_reply)
             return
 
