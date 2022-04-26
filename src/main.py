e01913edd6f8d9d8e9d5851e136d9b5f5b9c8b8c
Alfayed <baksha97@gmail.com>
2022-04-26 18:00:57 -0400

    fixes help command prefix

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ from dotenv import dotenv_values
 from aiconfig import generate_response, AVAILABLE_ENGINES
 
 config = dotenv_values(".env")
+COMMAND_PREFIX = "!"
 OPEN_AI_TOKEN = config.get('OPEN_AI_TOKEN')
 DISCORD_BOT_TOKEN = config.get('DISCORD_BOT_TOKEN')
 
@@ -19,18 +20,18 @@ async def on_ready():
 
 @bot.event
 async def on_message(message):
-    if message.content.startswith('help'):
+    if message.content.startswith(f'{COMMAND_PREFIX}help'):
         message_reply = ''
         for engine in AVAILABLE_ENGINES:
-            message_reply += f'`!{engine.name}`: {engine.description}\n'
-        message_reply += 'Usage: `!<engine_name> <actual prompt>`.\n'
+            message_reply += f'`{COMMAND_PREFIX}{engine.name}`: {engine.description}\n'
+        message_reply += f'Usage: `{COMMAND_PREFIX}<engine_name> <actual prompt>`.\n'
         message_reply += 'Costs: `davinci` > `curie` > `babbage` > `ada`.'
         await message.channel.send(message_reply)
         return
 
     for engine in AVAILABLE_ENGINES:
-        if message.content.startswith(f'!{engine.name} '):
-            prompt = message.content.replace(f'!{engine.name} ', '')
+        if message.content.startswith(f'{COMMAND_PREFIX}{engine.name} '):
+            prompt = message.content.replace(f'{COMMAND_PREFIX}{engine.name} ', '')
             message_reply = generate_response(prompt=prompt, engine=engine)
             await message.channel.send(message_reply)
             return
